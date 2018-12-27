815ee51c06f9154addd13015d2d6629bc2881f1a
Abdelkhalek El Darissi <abdelkhalaq@live.be>
2018-12-27 14:53:52 +0100

    commit de mon projet app2

---
+++ App2/App2/App2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(email.Text) || string.IsNullOrEmpty(password.Text))
+            {
+                titre.Text = "Veuillez entrer vos coordonnées!";
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
